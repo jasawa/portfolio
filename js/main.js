455641a06f8b882f0d8b4f51a6ad2640f7fb927d
Jane Asawa <asawajane@gmail.com>
2019-01-03 14:20:48 -0800

    trying new version of portfolio section added code to index.html,main.js and style.css

--- js/main.js
+++ js/main.js
@@ -61,4 +61,17 @@ Menu
 
 })();
 
+/*
+for new portfolio section
+*/
 
+$("[rel='tooltip']").tooltip();    
+ 
+$('.thumbnail').hover(
+	function(){
+		$(this).find('.caption').slideDown(250); //.fadeIn(250)
+	},
+	function(){
+		$(this).find('.caption').slideUp(250); //.fadeOut(205)
+	}
+); 
